5814467f03bc2a89a7686d04c0b85ebf51fb18ce
root <root@Default_Centos7.9>
2022-11-18 15:31:43 +0800

    Ver.2

--- VM.sh
+++ VM.sh
@@ -6,10 +6,10 @@ echo "----------wellcome to VM.sh----------"
 
 function lobby(){ #lobby functon
 echo -e "${RED}choose your function ${NC}\n${Yellow}(1)network\n(2)sshd_hosts.deny\n(3)NTP\n(4)ssh_password ${NC}"
-read answer
+read -e  answer
 case $answer in
     1)
-echo -e "\033[33mYou choose network Y/N: \033[0m" && read  a
+echo -e "\033[33mYou choose network Y/N: \033[0m" && read -e  a
 
 if [ "$a" == "Y" ]
 then
@@ -69,13 +69,13 @@ done
 
 
 function network(){
-echo -e "\033[33mHow many NIC? : \033[0m" && read NIC
+echo -e "\033[33mHow many NIC? : \033[0m" && read -e NIC
 if [ $NIC == 1 ] ;then
-echo -e "\033[33minput your ip: \033[0m" && read ip
+echo -e "\033[33minput your ip: \033[0m" && read -e ip
 check_ip
-echo -e "\033[33minput your mask ex(24) : \033[0m" && read mask
-echo -e "\033[33minput your GW : \033[0m" && read gw
-echo -e "\033[33minput your DNS : \033[0m" && read dns
+echo -e "\033[33minput your mask ex(24) : \033[0m" && read -e mask
+echo -e "\033[33minput your GW : \033[0m" && read -e gw
+echo -e "\033[33minput your DNS : \033[0m" && read -e dns
 nmcli con modify eth0 ipv4.addresses $ip/$mask ipv4.gateway $gw ipv4.dns $dns ipv4.method manual
 nmcli con up eth0 >/dev/null
 
@@ -83,20 +83,20 @@ elif [ $NIC == 2 ] ;then
 nmcli con show | grep eth0 >/dev/null && nmcli con show |grep eth1 >/dev/null
 if [ $? == 0 ];then
 #ch eth0
-echo -e "\033[33minput your eth0 ip : \033[0m" && read ip
+echo -e "\033[33minput your eth0 ip : \033[0m" && read -e ip
 check_ip
-echo -e "\033[33minput your mask ex(24) : \033[0m" && read mask
-echo -e "\033[33minput your GW : \033[0m" && read gw
-echo -e "\033[33minput your DNS : \033[0m" && read dns
+echo -e "\033[33minput your mask ex(24) : \033[0m" && read -e mask
+echo -e "\033[33minput your GW : \033[0m" && read -e gw
+echo -e "\033[33minput your DNS : \033[0m" && read -e dns
 echo -e "\033[35m------eth0------\nip  :$ip \nGW  :$gw\nDNS :$dns \033[0m"
 nmcli con modify eth0 ipv4.addresses $ip/$mask ipv4.gateway $gw ipv4.dns $dns ipv4.method manual
 nmcli con up eth0 >/dev/null
 #ch eth1
-echo -e "\033[33minput your eth1 ip : \033[0m" && read ip
+echo -e "\033[33minput your eth1 ip : \033[0m" && read -e ip
 check_ip
-echo -e "\033[33minput your mask ex(24) : \033[0m" && read mask
-echo -e "\033[33minput your GW : \033[0m" && read gw
-echo -e "\033[33minput your DNS : \033[0m" && read dns
+echo -e "\033[33minput your mask ex(24) : \033[0m" && read -e mask
+echo -e "\033[33minput your GW : \033[0m" && read -e gw
+echo -e "\033[33minput your DNS : \033[0m" && read -e dns
 echo -e "\033[35m------eth1------\nip  :$ip \nGW  :$gw\nDNS :$dns \033[0m"
 nmcli con modify eth1 ipv4.addresses $ip1/$mask ipv4.gateway $gw ipv4.dns $dns ipv4.method manual
 nmcli con up eth1 >/dev/null
